d44b500a4df61853ab498f76ee0ee1036dc719e2
DaviihRodrigueSs <davi.devrodrigues@gmail.com>
2024-03-06 13:08:44 -0300

    MembrosEstaticos2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+namespace MembrosEstaticos2
+{
+    class program
+    {
+        //definição do valor de PI
+       
+        static void Main(String[] args)
+            
+        {
+            //calculadoras diferentes não mudam o resultado.
+
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência:" + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+
+      
+        }
+    }
+}
